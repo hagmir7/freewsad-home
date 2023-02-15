812516c7d78351c1da39ff18aeae9f80585f8c93
Agmir Hassane <hagmir6@gmail.com>
2023-02-16 00:39:42 +0100

    firt commit

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -283,19 +283,11 @@ export const Book = () => {
                 googleAdId="ca-pub-6043226569102012"
               />
               <div className="p-2 mt-3 bg-light card shadow-sm border">
-                <span className="fst-italic h4 p-1">{t("Copyrights")}</span>
+                <span className="h4 p-1">{t("Copyrights")}</span>
                 <div>
-                  We respect the property rights of others, and are always
-                  careful not to infringe on their rights, so authors and
-                  publishing houses have the right to demand that an article or
-                  book download link be removed from the site. If you find an
-                  article or book of yours and do not agree to the posting of a
-                  download link, or you have a suggestion or complaint, write to
-                  us through the <Link to="/contact">{t("Contact Us")}</Link>,
-                  or by e-mail at:{" "}
-                  <a href="mailto:support@freewsad.com">
-                    support@freewsad.com.
-                  </a>
+                  {t("copyrights_text")} <Link to="/contact">{t("Contact Us")}</Link>,
+                  {" "} {t("or by email at")} : {" "}
+                  <a href="mailto:support@freewsad.com"> support@freewsad.com.</a>
                 </div>
                 <Link to="/about">{t("Read More")}</Link>
               </div>
