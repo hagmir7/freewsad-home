92513aa136405f84ff10594303472ff0c3ab0292
Agmir Hassane <hagmir6@gmail.com>
2023-03-18 23:20:20 +0100

    first commit

--- src/components/Book/ShearModal.js
+++ src/components/Book/ShearModal.js
@@ -33,8 +33,15 @@ export default function ShearModal(props) {
     }
     return (
         <>
-            <div className='justify-content-center d-flex'><button onClick={showModal} class="btn-primary btn rounded-pill col-md-6 mt-2" >{t("Shear This Book")}</button></div>
-            <Modal title={t("Shear This Book")} open={isModalOpen} onCancel={handleCancel} footer={null} >
+            <div className='justify-content-center d-flex'>
+                <button onClick={showModal} class="btn-primary btn rounded-pill col-md-6 mt-2" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-share" viewBox="0 0 16 16">
+                        <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
+                    </svg>
+                    &#xa0;&#xa0;{t("Share This Book")} &#xa0;&#xa0;
+                 </button>
+            </div>
+            <Modal title={t("Share This Book")} open={isModalOpen} onCancel={handleCancel} footer={null} >
                 <ul className='d-flex justify-content-around p-3 list-unstyled'>
                     <li>
                     <a style={{color:"#25D366"}} href={`whatsapp://send?text=${props.title + " " + encodeURIComponent(window.location.href)}`} target="_blank">
